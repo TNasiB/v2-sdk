a98eb7fe0e136bd6d8a4fc1c556b259253cc2a66
Тимерьян Насибуллин <-github-email@example.com>
2024-07-02 20:18:47 +0500

    feat: upd

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,7 @@ export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
 
 export const FACTORY_ADDRESS_MAP: { [chainId: number]: string } = V2_FACTORY_ADDRESSES
 
-export const INIT_CODE_HASH = '9417ef8aa850b961af944d0ecee70dc0cab6363a90a42a708e680b70b6a5e1dc'
+export const INIT_CODE_HASH = '0x9417ef8aa850b961af944d0ecee70dc0cab6363a90a42a708e680b70b6a5e1dc'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
